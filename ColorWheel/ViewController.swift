7c6e9656edf51932fbf5ed3065d87852949637e8
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2018-02-11 14:05:51 -0500

    KO - changed size of arcs to 90.0 to make a whole 360 circle (yay geometry!), took size out and made into size constant

--- ColorWheel/ViewController.swift
+++ ColorWheel/ViewController.swift
@@ -16,12 +16,18 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(true)
+        let size = CGFloat(90.0)
+        //break size out into variable since it's all the same...later see if there's a prettier way to do this... hmm...
+
+        circleSegmentView.center.x = self.view.center.x
+        circleSegmentView.center.y = self.view.center.y
         circleSegmentView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 200)
         circleSegmentView.arcs = [
-            Arc(color: UIColor.red.cgColor, size: 25.0),
-            Arc(color: UIColor.blue.cgColor, size: 25.0),
-            Arc(color: UIColor.green.cgColor, size: 25.0),
-            Arc(color: UIColor.blue.cgColor, size: 25.0)
+            
+            Arc(color: UIColor.red.cgColor, size: size),
+            Arc(color: UIColor.blue.cgColor, size: size),
+            Arc(color: UIColor.green.cgColor, size: size),
+            Arc(color: UIColor.purple.cgColor, size: size)
         ]
         
         view.addSubview(circleSegmentView)
