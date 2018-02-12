b0c4a12f1b9a44877fd12882ded17e8e9f1dd6bd
Katherine Owens <katherineowens@Katherines-MacBook-Pro.local>
2018-02-11 23:37:12 -0500

    ko - started watch extension

--- ColorWheel WatchKit Extension/ColorWheelInterfaceController.swift
+++ ColorWheel WatchKit Extension/ColorWheelInterfaceController.swift
@@ -1,31 +1,36 @@
 //
-//  InterfaceController.swift
+//  ColorWheelInterfaceController.swift
 //  ColorWheel WatchKit Extension
 //
-//  Created by Katherine Owens on 2/9/18.
+//  Created by Katherine Owens on 2/11/18.
 //  Copyright © 2018 Katherine Owens. All rights reserved.
 //
 
 import WatchKit
 import Foundation
 
-
-class InterfaceController: WKInterfaceController {
-
+class ColorWheelInterfaceController: WKInterfaceController {
+    @IBOutlet weak var circleGroup: WKInterfaceGroup!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
-        // Configure interface objects here.
+        setUpCircle()
     }
-    
+
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
     }
-    
+
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
 
+    func setUpCircle() {
+        circleGroup?.setBackgroundColor(UIColor.red)
+        circleGroup?.setCornerRadius(50.0)
+        circleGroup?.setHidden(false)
+    }
 }
